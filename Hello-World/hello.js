93244515a2b2dcab2bdf3c67855db23fc1edd018
meghana123-k <kothakondameghana3@gmail.com>
2026-07-07 22:40:37 +0530

    refactor: update hello.js to use ES6 modules and add math functions

--- Hello-World/hello.js
+++ Hello-World/hello.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+// const math = require("./math");
+// console.log(add); // add function
+
+// console.log(math.add(5, 6));
+// console.log(math.sub(5, 6));
+
+// const { add, sub } = require("./math");
+
+// console.log(add); // anonymous function
+
+// console.log(add(5, 2));
+// console.log(sub(5, 2));
+
+import { add, sub } from "./math.js";
+console.log(add(2, 3));
+console.log(sub(12, 3));
---
+++ Hello-World/math.js
@@ -0,0 +1,24 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// // module.exports = {
+// //   addFn: add,
+// //   subFn: sub,
+// // };
+// module.exports = { add, sub };
+
+// exports.add = (a, b) => a + b;
+
+// exports.sub = (a, b) => a - b;
+
+export function add(a, b) {
+    return a + b;
+}
+export function sub(a, b) {
+    return a - b;
+}
