5978ec118725788dbc093ed9eb7fead20b0bd80b
meghana123-k <kothakondameghana3@gmail.com>
2026-09-07 18:06:58 +0530

    feat: enhance server routing with product information handling and add URL dependency

--- Practice-Node/HTTP_Practice/app.js
+++ Practice-Node/HTTP_Practice/app.js
@@ -1,7 +1,8 @@
 const http = require("http");
-
+const url = require("url");
 const myServer = http.createServer((req, res) => {
-  switch (req.url) {
+  const myUrl = url.parse(req.url, true);
+  switch (myUrl.pathname) {
     case "/":
       res.statusCode = 200;
       res.end("Welcome to my Server");
@@ -13,6 +14,18 @@ const myServer = http.createServer((req, res) => {
     case "/contact":
       res.statusCode = 200;
       res.end("Contact us at contact@example.com");
+      break;
+    case "/products":
+      const productName = myUrl.query.name;
+      const price = myUrl.query.price;
+      if (productName === undefined || price === undefined) {
+          res.statusCode = 400;
+          res.end("Missing Product Information");
+          return;
+        } else {
+          res.statusCode = 200;
+          res.end(`Product: ${productName}\nPrice: ${price}`);
+      }
       break;
     default:
       res.statusCode = 404;
