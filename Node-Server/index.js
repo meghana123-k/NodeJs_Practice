d94f788e2cf83c2abad274932ea777cccbe26e00
meghana123-k <kothakondameghana3@gmail.com>
2026-09-07 15:16:07 +0530

    feat: initialize Node.js server with request handling and logging

---
+++ Node-Server/index.js
@@ -0,0 +1,43 @@
+const { log } = require("console");
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+  // log("New Request Recieved");
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} : ${req.url} : New Request\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+  
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Home Page");
+        break;
+
+      case "/about":
+        const username = myUrl.query.name;
+        res.end(`Hi! ${username}`);
+        break;
+
+      case "/search":
+        const search_query = myUrl.query.search_query;
+        if(search_query=== undefined) {
+          res.end("Enter valid query parameter");
+          return;
+        }
+        res.end("Here are your results for "+search_query)
+        break;
+      case "/contact":
+        res.end("Contact Page");
+        break;
+      default:
+        res.end("Error page");
+    }
+  });
+});
+
+myServer.listen(8000, () => {
+  log("Server started...");
+});
